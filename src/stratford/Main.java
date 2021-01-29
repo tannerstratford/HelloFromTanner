b904554ec7c62496ea8e9c129bc11da490411874
Jeffery Johnson <joh20117@byui.edu>
2021-01-28 20:25:30 -0600

    Updated as required.

--- src/stratford/Main.java
+++ src/stratford/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Tanner");
+        System.out.println("Hello, Tanner, my name is Jeffery");
     }
 }
